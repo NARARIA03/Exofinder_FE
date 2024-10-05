6dc3523da5277ff0ab6cd71c64a69a43f3d359e1
NARARIA03 <hyeonseong0305@gmail.com>
2024-10-06 01:20:22 +0900

    Chore: gh-pages setting complete

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/FrontEnd/",
   resolve: {
     alias: [
       { find: "@apis", replacement: path.resolve(__dirname, "src/apis") },
