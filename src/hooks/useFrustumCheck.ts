8ed427ee403816e0d3334c7a4ff4fe4ac2762745
NARARIA03 <hyeonseong0305@gmail.com>
2024-10-05 16:09:17 +0900

    Fix: Calc useFrustumCheck init render
    - Resolved issue where visibleExoplanet and visibleStarCount
      were not calculated immediately after initial render

--- src/hooks/useFrustumCheck.ts
+++ src/hooks/useFrustumCheck.ts
@@ -5,8 +5,7 @@ import {
   visibleExoplanetAtom,
   visibleStarCountAtom,
 } from "../store/jotai";
-import { useRef } from "react";
-import { useFrame } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
 
 export const useFrustumCheck = (camera: PerspectiveCamera, scene: Scene) => {
   const diameter = useAtomValue(diameterAtom);
@@ -17,54 +16,72 @@ export const useFrustumCheck = (camera: PerspectiveCamera, scene: Scene) => {
   const prevCamMatrix = useRef(new Matrix4());
   const prevDiameter = useRef(6);
 
-  useFrame(() => {
-    const currentCameraMatrix = new Matrix4().copy(camera.matrixWorld);
+  useEffect(() => {
+    const calcFrustumCheck = () => {
+      const currentCameraMatrix = new Matrix4().copy(camera.matrixWorld);
 
-    // 카메라가 이전과 달라진 경우 && diameter 값이 바뀐 경우만 카메라 범위 내 행성 정보 업데이트
-    if (
-      !currentCameraMatrix.equals(prevCamMatrix.current) ||
-      diameter !== prevDiameter.current
-    ) {
-      const frustum = new Frustum();
-      const cameraViewProjectionMatrix = new Matrix4();
+      // 카메라가 이전과 달라진 경우 && diameter 값이 바뀐 경우만 카메라 범위 내 행성 정보 업데이트
+      if (
+        !currentCameraMatrix.equals(prevCamMatrix.current) ||
+        diameter !== prevDiameter.current
+      ) {
+        const frustum = new Frustum();
+        const cameraViewProjectionMatrix = new Matrix4();
 
-      // 카메라의 뷰 프로젝션 매트릭스를 얻어서 frustum 업데이트
-      camera.updateMatrixWorld(); // 카메라의 매트릭스 업데이트
-      camera.updateProjectionMatrix();
-      cameraViewProjectionMatrix.multiplyMatrices(
-        camera.projectionMatrix,
-        camera.matrixWorldInverse
-      );
-      frustum.setFromProjectionMatrix(cameraViewProjectionMatrix);
+        // 카메라의 뷰 프로젝션 매트릭스를 얻어서 frustum 업데이트
+        camera.updateMatrixWorld(); // 카메라의 매트릭스 업데이트
+        camera.updateProjectionMatrix();
+        cameraViewProjectionMatrix.multiplyMatrices(
+          camera.projectionMatrix,
+          camera.matrixWorldInverse
+        );
+        frustum.setFromProjectionMatrix(cameraViewProjectionMatrix);
 
-      // 씬 안의 모든 오브젝트의 경계와 비교
-      const objectsInView: Mesh[] = [];
-      scene.traverse((object) => {
-        if (object instanceof Mesh && object.visible) {
-          const objectBoundingBox = new Box3().setFromObject(object);
-          if (frustum.intersectsBox(objectBoundingBox)) {
-            objectsInView.push(object);
+        // 씬 안의 모든 오브젝트의 경계와 비교
+        const objectsInView: Mesh[] = [];
+        scene.traverse((object) => {
+          if (object instanceof Mesh && object.visible) {
+            const objectBoundingBox = new Box3().setFromObject(object);
+            if (frustum.intersectsBox(objectBoundingBox)) {
+              objectsInView.push(object);
+            }
           }
-        }
-      });
-      if (objectsInView.length !== visibleExoplanet.length) {
-        setVisibleExoplanet(() =>
-          objectsInView
-            .map((mesh) => mesh.userData.planetName)
-            .filter(
-              (planetName) => planetName !== null && planetName !== undefined
-            )
-        );
-        setVisibleStarCount(
-          () =>
+        });
+        if (objectsInView.length !== visibleExoplanet.length) {
+          setVisibleExoplanet(() =>
             objectsInView
-              .map((mesh) => mesh.userData.starName)
-              .filter((starName) => starName !== null && starName !== undefined)
-              .length
-        );
+              .map((mesh) => mesh.userData.planetName)
+              .filter(
+                (planetName) => planetName !== null && planetName !== undefined
+              )
+          );
+          setVisibleStarCount(
+            () =>
+              objectsInView
+                .map((mesh) => mesh.userData.starName)
+                .filter(
+                  (starName) => starName !== null && starName !== undefined
+                ).length
+          );
+        }
+        prevCamMatrix.current.copy(currentCameraMatrix);
+        prevDiameter.current = diameter;
       }
-      prevCamMatrix.current.copy(currentCameraMatrix);
-      prevDiameter.current = diameter;
-    }
-  });
+    };
+
+    const interval = setInterval(() => {
+      calcFrustumCheck();
+    }, 50);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [
+    camera,
+    diameter,
+    scene,
+    setVisibleExoplanet,
+    setVisibleStarCount,
+    visibleExoplanet.length,
+  ]);
 };
