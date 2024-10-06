99cd5f96f274fe92f375c330045c8b8dc50d0624
NARARIA03 <hyeonseong0305@gmail.com>
2024-10-06 16:00:55 +0900

    Fix: hide <SpecType /> when an exoplanet is select
    - Updated <SpecType /> to only render when clickExoplanetName atom
      is an empty string ("").
    - Ensures the component is hidden when an exoplanet is selected.

--- src/components/modal/SpecType.tsx
+++ src/components/modal/SpecType.tsx
@@ -1,9 +1,14 @@
 import QuestionComp from "@components/tooltip/QuestionComp";
-import { hoverSpecTypeAtom, spectralCountAtom } from "@store/jotai";
+import {
+  clickExoplanetNameAtom,
+  hoverSpecTypeAtom,
+  spectralCountAtom,
+} from "@store/jotai";
 import { useAtomValue, useSetAtom } from "jotai";
 
 export default function SpecType() {
   const setHoverSpecType = useSetAtom(hoverSpecTypeAtom);
+  const clickExoplanetName = useAtomValue(clickExoplanetNameAtom);
   const spectralCount = useAtomValue(spectralCountAtom);
   const specType = ["O", "B", "A", "F", "G", "K", "M"];
 
@@ -14,6 +19,8 @@ export default function SpecType() {
   const handleMouseLeave = () => {
     setHoverSpecType("");
   };
+
+  if (clickExoplanetName) return null;
 
   return (
     <div className="fixed flex p-3 flex-col top-3 left-3 bg-slate-400 rounded-xl justify-center items-start z-50">
